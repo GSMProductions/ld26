3e9d9413b694ba0915e120fdcac0e2e4449e6347
Ephy Orven <fyinphy@hotmail.com>
2013-04-27 18:43:12 +0200

    main name

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from behaviour import MoveCharacter
 
 def test_combat():
 
-    heros = [Character('nod',(0,0),[20,20],[20,20]),Character('nel',(0,0),[20,20],[20,20])]
+    heros = [Character('nod1',(0,0),[20,20],[20,20]),Character('nel1',(0,0),[20,20],[20,20])]
     dummy_scene = FightScene('prairie',heros)
     cocos.director.director.push(dummy_scene)
 
 def test_map():
-    player = Character('nod',(0,0),[20,20],[20,20])
+    player = Character('nod1',(0,0),[20,20],[20,20])
     player.do(MoveCharacter())
 
     dummy_scene = Map('maptest')
