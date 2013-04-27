e2133c3079e57e9afaaf87cbb09d81cbf26b4022
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-04-27 13:03:33 +0200

    Import a test map in tiled format

---
+++ map.py
@@ -0,0 +1,17 @@
+
+import cocos
+
+class Map(cocos.scene.Scene):
+
+    def __init__(self, name):
+
+        self.name = name
+
+        self.scroller = cocos.layer.ScrollingManager()
+        self.char_layer = cocos.layer.ScrollableLayer()
+        self.map_layer = cocos.tiles.load('maps/'+self.name+'.tmx')["Tile Layer 1"]
+
+        self.scroller.add(self.map_layer)
+        self.scroller.add(self.char_layer)
+
+        cocos.scene.Scene.__init__(self, self.scroller)
