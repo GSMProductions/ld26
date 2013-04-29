156e2450e9bf0b5158c5400b621190dd781f66eb
Thomas Stassin <thomas.stassin@gmsproductions.com>
2013-04-29 14:28:56 +0200

    ajout de l'inventaire

---
+++ inventory.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import cocos
+import pyglet
+
+
+class Inventory(dict):
+
+    def add(self, obj):
+        
+        if self.has_key(obj):
+            self[obj] += 1
+
+        else:
+            self[obj] = 1
+
+    def remove(self,obj):
+
+        if self.has_key(obj):
+            self[obj] -= 1
+
+
+
