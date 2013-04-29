701adc731830a4b31651ed9be818adbb8b2612e3
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2013-04-29 08:35:10 +0200

    Fichier manquant

---
+++ maps/tileset_grassland.tsx
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_grassland" tilewidth="32" tileheight="32">
+ <image source="tileset_grassland.png" width="640" height="640"/>
+ <tile id="14">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+ <tile id="300">
+  <properties>
+   <property name="door" value="True"/>
+   <property name="door_destination" value="4 1"/>
+   <property name="door_map" value="forest"/>
+   <property name="passable" value="True"/>
+  </properties>
+ </tile>
+</tileset>
